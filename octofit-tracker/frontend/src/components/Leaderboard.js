cac4c081737d7913d7d6cf9fc4398ba4d7cf741d
jbellver <jbellver@gmail.com>
2026-01-16 18:13:32 +0000

    misssin s

--- octofit-tracker/frontend/src/components/Leaderboard.js
+++ octofit-tracker/frontend/src/components/Leaderboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const API_URL = `https://${process.env.REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/leaderboards/`;
+const API_URL = `https://${process.env.REACT_APP_CODESPACE_NAME}-8000.app.github.dev/api/leaderboard/`;
 
 
 function Leaderboard() {
